caf867dd9eb0239f220160f63f20793339fef5f7
Kyle Chung (RD-TW) <kyle_chung@trend.com.tw>
2013-08-18 20:47:55 +0800

    update

--- ggplot2_exercise.R
+++ ggplot2_exercise.R
@@ -10,7 +10,10 @@ library(ggplot2)
 
 ## bar graph
 str(ChickWeight)
-ggplot(ChickWeight, aes(x=Diet,y=weight)) + geom_bar(stat='identity')
+ggplot(ChickWeight, aes(x=Diet,y=weight)) + 
+    geom_bar(stat='identity') +
+    xlab('This is my customized xlab!') + 
+    ylab('And this is ylab!')
 
 # in aes(), x denotes the categorical variable in the given data.frame, 
 # and y is the value to be plotted if stat='identity' is used
@@ -30,13 +33,19 @@ ggplot(check1, aes(x=Diet,y=weight)) +
     geom_bar(stat='identity', fill='blue', color='red')
 
 # x var can be factors or pure strings:
-check$Diet.str <- as.character(check$Diet)
+check1$Diet.str <- as.character(check1$Diet)
 ggplot(check1, aes(x=Diet.str,y=weight)) + geom_bar(stat='identity')
 
 # if x is numeric (i.e., continuous var), possible null will be inserted:
 ggplot(ChickWeight, aes(x=Time,y=weight)) + geom_bar(stat='identity')
 # to cure this (if preferred), use factor():
 ggplot(ChickWeight, aes(x=factor(Time),y=weight)) + geom_bar(stat='identity')
+
+# ignore y var to plot the x var by its counts:
+ggplot(ChickWeight, aes(x=Diet)) + geom_bar(stat='bin') # or simply geom_bar()
+# check if these are the correct counts:
+check3 <- aggregate(weight ~ Diet, data=ChickWeight, FUN=length)
+ggplot(check3, aes(x=Diet, y=weight)) + geom_bar(stat='identity')
 
 # a second-level x var (the fill var): result on y seems strange, bug?
 ggplot(ChickWeight, aes(x=factor(Time),fill=Diet,y=weight)) + 
@@ -55,12 +64,15 @@ ggplot(ChickWeight, aes(x=factor(Time),fill=Diet,y=weight)) +
     geom_bar(stat='identity') +
     scale_fill_brewer(palette='Pastel1')
 
-# ignore y var to plot the x var by its counts:
-ggplot(ChickWeight, aes(x=Diet)) + geom_bar(stat='bin') # or simply geom_bar()
-# check if these are the correct counts:
-check3 <- aggregate(weight ~ Diet, data=ChickWeight, FUN=length)
-ggplot(check3, aes(x=Diet, y=weight)) + geom_bar(stat='identity')
+# plot different bar with different colors
+# notice that this is different from using a secondary group-by var
+check2$cate <- ifelse(check2$Diet==1, 1, 0)
+ggplot(check2, aes(x=Diet,fill=factor(cate),y=weight)) + 
+    geom_bar(stat='identity')
 
+# re-order x var by its height (value of y var)
+ggplot(check2, aes(x=reorder(Diet,-weight),fill=factor(cate),y=weight)) + 
+    geom_bar(stat='identity')
 
 
 
